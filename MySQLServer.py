f32b7ed66741011e21b228b7d627776c2beb6a42
israa-hassanein <israa.hassanein@gmail.com>
2024-10-21 00:18:47 +0300

    task 1

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,25 +5,27 @@ def create_database():
     try:
         # Connect to MySQL server
         connection = mysql.connector.connect(
-            host='localhost',
-            user='root',  # Replace with your MySQL username
+            host='localhost',         # Replace with your host, e.g., 'localhost' or IP address
+            user='root',     # Replace with your MySQL username
             password='is711ib2012'  # Replace with your MySQL password
         )
-
+        
         if connection.is_connected():
             cursor = connection.cursor()
+
+            # Create the database if it doesn't already exist
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
-
+    
     except Error as e:
         print(f"Error: {e}")
-
+    
     finally:
-        # Close the connection
-        if connection.is_connected():
+        # Close the cursor and connection
+        if cursor:
             cursor.close()
+        if connection.is_connected():
             connection.close()
-            print("MySQL connection is closed")
 
 if __name__ == "__main__":
     create_database()
